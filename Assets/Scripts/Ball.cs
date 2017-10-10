2a1422851ccaaa4bac7bb901e2fd71c326dd9f7d
elninowang <elninowang@qq.com>
2017-10-10 20:25:02 +0800

    complete the project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Scripting;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    protected float jump_speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = new Vector3(-7.0f, 6.0f, 0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Scripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    protected float jump_speed = 8.0f;
+    public bool is_landing = false;
+
+	// Use this for initialization
+	void Start () {
+        this.is_landing = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.is_landing)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.is_landing = false;
+                GetComponent<Rigidbody>().velocity = Vector3.up * this.jump_speed;
+            }
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            this.is_landing = true;
+    }
+}
